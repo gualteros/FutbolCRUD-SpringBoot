4b9f97305dfd3e5e77c11ac3300d42ab7a21461a
Daniel Gualteros <daniel.gualteros.10@gmail.com>
2022-02-28 23:18:52 -0500

    Update

--- DB/init.sql
+++ DB/init.sql
@@ -12,13 +12,15 @@ Propietario VARCHAR(50),
 Capacidad LONG,
 Division INT(1),
 Competicion VARCHAR(50),
-numeroJugadores INT(3),
+Numero_Jugadores INT(3),
+Fecha_Fundacion TIMESTAMP, 
 Fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-INSERT INTO Futbol (Nombre, Ciudad, Propietario, Capacidad, Division, Competicion, numeroJugadores)
-VALUES  ("chelsea", "London", "Thomas Tuchel", 10000000, 1, "Premier League", 41),
-		("Santa Fe", "Bogotá D.C.", "Perlun S.A.S.", 100000, 1, "Liga BetPlay", 41),
-		("Liverpool", "Liverpool", "Fenway Sports Group", 10000022, 1, "Premier League", 31);
+INSERT INTO Futbol (Nombre, Ciudad, Propietario, Capacidad, Division, Competicion, Numero_Jugadores,Fecha_Fundacion)
+VALUES  ("chelsea", "London", "Thomas Tuchel", 10000000, 1, "Premier League", 41, "10-06-26"),
+		("Santa Fe", "Bogotá D.C.", "Perlun S.A.S.", 100000, 1, "Liga BetPlay", 41, "10-06-26"),
+		("Liverpool", "Liverpool", "Fenway Sports Group", 10000022, 1, "Premier League", 31, "10-06-26");
+
 
 SELECT * FROM db.Futbol;
